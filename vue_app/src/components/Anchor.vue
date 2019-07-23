af2b299156b64fe11f2bc2661b1b29129004a7f2
Craigz0rs <cdarcy@live.ca>
2019-07-23 14:08:43 -0700

    feat(anchor) intialized anchor component and added it to ContentTopper component

---
+++ vue_app/src/components/Anchor.vue
@@ -0,0 +1,19 @@
+<template>
+    <a v-bind:href="href" v-bind:target="target" v-bind:class="classes">{{ title }}</a>
+</template>
+
+<script>
+export default {
+    name: 'Anchor',
+    props: [
+        'href',
+        'target',
+        'classes',
+        'title'
+    ]
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- vue_app/src/components/ContentTopper.vue
+++ vue_app/src/components/ContentTopper.vue
@@ -3,12 +3,12 @@
         <div class="contact-tab" id="nav-tabs">
         <ul>
             <Button v-for="button in buttons" v-bind:key="button.id" v-bind:title="button.title" v-bind:classes="button.classes" />
-            <Button title="MY WORK" classes="navbutton" />
+            <Anchor v-for="anchor in anchors" v-bind:key="anchor.id" v-bind:title="anchor.title" v-bind:classes="anchor.classes" v-bind:target="anchor.target" v-bind:href="anchor.href" />
             <!-- <li><button class="navbutton nav-active" @click="showProjects($event)" id="about-toggle">ABOUT ME</button></li>
             <li><button class="navbutton" @click="showProjects($event)" id="project-toggle">MY WORK</button></li> -->
-            <li><a target="_blank" href="http://www.linkedin.com/in/craigdarcy" class="social secondary"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
+            <!-- <li><a target="_blank" href="http://www.linkedin.com/in/craigdarcy" class="social secondary"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
             <li><a target="_blank" href="https://github.com/Craigz0rs" class="social secondary"><i class="fa fa-github-square" aria-hidden="true"></i></a></li>
-            <li><a href="mailto:hello@craigdarcy.ca" class="email secondary"><i class="fa fa-envelope" aria-hidden="true"></i></a><br class="responsive-break"></li>
+            <li><a href="mailto:hello@craigdarcy.ca" class="email secondary"><i class="fa fa-envelope" aria-hidden="true"></i></a><br class="responsive-break"></li> -->
         </ul>
         </div>
     </div> 
@@ -16,15 +16,23 @@
 
 <script>
 import Button from "./Button.vue"
+import Anchor from "./Anchor.vue"
+
 export default {
     name: 'ContentTopper',
     components: {
-        Button
+        Button,
+        Anchor
     },
     data() {
         return {
             buttons: [
-                { id: "button1", title: "blah", classes: "blargh" }
+                { id: "button1", title: "ABOUT ME", classes: "navbutton" },
+                { id: "button2", title: "MY WORK", classes: "navbutton" }
+            ],
+
+            anchors: [
+                { id: "anchor1", title: "<i aria-hidden='true' class='fa fa-linkedin-square'></i>", target: "_blank", href: "http://www.linkedin.com/in/craigdarcy", classes: "social secondary" }
             ]
         }
     }
