99f75ac2285267516925685e936942450c57578b
Craigz0rs <cdarcy@live.ca>
2019-07-24 02:08:54 -0700

    feat(AboutMe): initialized and imported AboutMe component to MainContent component

--- vue_app/src/components/AboutMe.vue
+++ vue_app/src/components/AboutMe.vue
@@ -1,10 +1,27 @@
 <template>
-    
+    <section id="about-me" class="fadeIn trigger">
+        <h1>About me</h1>
+        <p>
+            Back in 2000 I built my first website when I was 10 years old. I signed up for Geocities and made one of those hideous old websites complete with a visitor counter and a guestbook. I titled it "Craig's Pokémon Website" and hot linked (sorry!) some Pokémon pictures from around the net. It was pretty much a vertical stack of pixelated 256-colour Pokémon pictures on a bright red background with lime green text. I thought it was pretty cool and I even got hundreds of visitors right up until my account was terminated for a "Terms of Service" violation because you needed to be 13 to use Geocities.
+        </p>
+        <p>
+        It was a couple years later when I started experimenting with QBasic. At the time I had no resources other than my brother, so it didn't go much further than printing text to the console and changing its colour and size and designing icons for the executable files. I did get some resources from the library at one point which allowed me to make some simple games by copying the code right out of the book. I started to get ambitious and wanted to make real applications in Windows, so not knowing anything I downloaded some C++ IDE and thought I could just use Basic syntax and would create the next SimCity. No surprise that nothing would compile, it was very frustrating, there were no online resources like there are now so I wrote coding off as something only computer geniuses could do.
+        </p>
+        <p>
+        Fast forward to recent years; I had been interested in design and development for many years but didn't think that coding was something I would be able to learn. After meeting people in the industry and looking up modern coding resources online I discovered that it was something anyone could learn. I enrolled in some Java Application Development courses at BCIT to see if it was for me and gained some foundational skills in OOP. I then enrolled in BCIT's Technical Web Design program and learned to design and build fully functional websites in only a few months. Since I finished that program I have worked on projects for real clients and made some fun side projects for my own learning.
+        </p>
+        <p>
+            Outside of my design and development interests you can find me pursuing my fitness goals, Netflix binging, and eating burgers.
+        </p>
+        <p>
+            I am currently seeking front-end developer roles in the Vancouver and Calgary regions.
+        </p>
+    </section>
 </template>
 
 <script>
 export default {
-    
+    name: "about-me",
 }
 </script>
 
