41b922223851702e91e5879f8b0d6d5f7b663cde
Craigz0rs <cdarcy@live.ca>
2019-07-25 00:32:47 -0700

    refactor((aboutme)): rename AboutMe component to SectionAboutMe

--- vue_app/src/components/TheMainContent.vue
+++ vue_app/src/components/TheMainContent.vue
@@ -1,21 +1,21 @@
 <template>
     <div class="content">
         <TheContentTopper />
-        <AboutMe />
+        <SectionAboutMe />
         <ProjectList />
     </div>
 </template>
 
 <script>
 import TheContentTopper from "./TheContentTopper.vue"
-import AboutMe from "./AboutMe.vue"
+import SectionAboutMe from "./SectionAboutMe.vue"
 import ProjectList from "./ProjectList.vue"
 
 export default {
     name: 'TheMainContent',
     components: {
         TheContentTopper,
-        AboutMe,
+        SectionAboutMe,
         ProjectList
     }
 };
