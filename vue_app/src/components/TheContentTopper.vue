2fe3bedb21d071eb0fb9b527927d06d3332233af
Craigz0rs <cdarcy@live.ca>
2019-07-26 19:19:49 -0700

    refactor(thecontenttopper): removed social link objects, cleaned code styling

--- vue_app/src/components/TheContentTopper.vue
+++ vue_app/src/components/TheContentTopper.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="content__topper">
-    <div class="contact-tab" id="nav-tabs">
+    <div class="nav__wrap">
       <ul>
         <li v-for="button in buttons" :key="button.id">
-          <BaseButton v-html="button.title" :classes="button.classes" @click.native="toggle($event)" />
+          <BaseButton
+            v-html="button.title"
+            :classes="button.classes"
+            @click.native="toggle($event)"
+          />
         </li>
       </ul>
-      <SocialAnchorListing :ul-class="'--main-nav'" />
-        <!-- <li><button class="navbutton nav-active" @click="showProjects($event)" id="about-toggle">ABOUT ME</button></li>
-        <li><button class="navbutton" @click="showProjects($event)" id="project-toggle">MY WORK</button></li>-->
-        <!-- <li><a target="_blank" href="http://www.linkedin.com/in/craigdarcy" class="social secondary"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
+      <SocialAnchorListing :ul-class="'--social'" />
+      <!-- <li><button class="navbutton nav-active" @click="showProjects($event)" id="about-toggle">ABOUT ME</button></li>
+      <li><button class="navbutton" @click="showProjects($event)" id="project-toggle">MY WORK</button></li>-->
+      <!-- <li><a target="_blank" href="http://www.linkedin.com/in/craigdarcy" class="social secondary"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
             <li><a target="_blank" href="https://github.com/Craigz0rs" class="social secondary"><i class="fa fa-github-square" aria-hidden="true"></i></a></li>
-        <li><a href="mailto:hello@craigdarcy.ca" class="email secondary"><i class="fa fa-envelope" aria-hidden="true"></i></a><br class="responsive-break"></li>-->
+      <li><a href="mailto:hello@craigdarcy.ca" class="email secondary"><i class="fa fa-envelope" aria-hidden="true"></i></a><br class="responsive-break"></li>-->
     </div>
   </div>
 </template>
@@ -31,54 +35,39 @@ export default {
   data() {
     return {
       buttons: [
-        { id: "button1", title: "ABOUT ME", classes: ["navbutton", "nav-active"], isActive: true },
-        { id: "button2", title: "MY WORK", classes: ["navbutton"], isActive: false }
-      ],
-
-
-      anchors: [
         {
-          id: "anchor1",
-          title: "<i aria-hidden='true' class='fa fa-linkedin-square'></i>",
-          target: "_blank",
-          href: "http://www.linkedin.com/in/craigdarcy",
-          classes: "link--social secondary"
+          id: "button1",
+          title: "ABOUT ME",
+          classes: ["navbutton", "nav-active"],
+          isActive: true
         },
         {
-          id: "anchor2",
-          title: "<i class='fa fa-github-square' aria-hidden='true'></i>",
-          target: "_blank",
-          href: "https://github.com/Craigz0rs",
-          classes: "link--social secondary"
-        },
-        {
-          id: "anchor3",
-          title: "<i class='fa fa-envelope' aria-hidden='true'></i>",
-          target: "_blank",
-          href: "mailto:hello@craigdarcy.ca",
-          classes: "link--social secondary"
+          id: "button2",
+          title: "MY WORK",
+          classes: ["navbutton"],
+          isActive: false
         }
       ]
     };
   },
   methods: {
     toggle(event) {
-      console.log(event)
-      this.toggleButtonActive(event)
+      console.log(event);
+      this.toggleButtonActive(event);
     },
     toggleButtonActive() {
       this.isActive = !this.isActive;
-      console.log(this)
+      console.log(this);
       // if(this.classes.includes("nav-active")) {
       //   let i = this.classes.indexOf(this.classes.includes("nav-active"));
-        
+
       // }
     }
   }
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .content__topper {
   height: 500px;
   width: 200vw;
@@ -89,7 +78,9 @@ export default {
   background-color: white;
 }
 
-.contact-tab {
+.nav__wrap {
+  display: flex;
+  flex-wrap: nowrap;
   position: absolute;
   top: -47px;
   right: 104vw;
@@ -97,7 +88,6 @@ export default {
   ul {
     display: flex;
     justify-content: right;
-    padding-right: 5%;
   }
 }
 </style>
