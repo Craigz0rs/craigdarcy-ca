9b6d43c603fb0e1d10859fdd224340ddf7283a85
Craigz0rs <cdarcy@live.ca>
2019-07-31 03:43:03 -0700

    chore(raw-loader): add raw-loader dependency to project

--- vue_app/vue.config.js
+++ vue_app/vue.config.js
@@ -8,5 +8,13 @@ module.exports = {
             `
       }
     }
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('raw')
+      .test(/\.txt$/)
+      .use('raw-loader')
+      .loader('raw-loader')
+      .end()
   }
 };
