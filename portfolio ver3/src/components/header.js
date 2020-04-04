340a1c0c92c52c3777b141bfe3516342a309ed12
Craigz0rs <cdarcy@live.ca>
2020-04-04 00:26:11 -0700

    wip(header): signature spring animation

--- portfolio ver3/src/components/header.js
+++ portfolio ver3/src/components/header.js
@@ -6,7 +6,7 @@ import Overlay from "../images/header__overlay.svg"
 import { useSpring, animated } from 'react-spring'
 
 const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2]
-const trans1 = (x, y) => `translate3d(${x / 10}px,${y / 10}px,0)`
+const signature = (x, y) => `translate3d(${x / -175 }px,${y / 200}px,0) rotate(-5deg)`
 const trans2 = (x, y) => `translate3d(${x / 85}px,${y / 1000}px,0) scale(1.1)`
 const trans3 = (x, y) => `translate3d(${x / 100}px,${y / 50}px,0) scale(2) rotate(3deg)`
 const trans4 = (x, y) => `translate3d(${x / 10 - 1}px,${y / 100}px,0) scale(2) rotate(-12deg) rotateY(180deg)`
@@ -16,9 +16,9 @@ function Header() {
   return (
   <header className="header" onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
     <div className="grid content-wrap">
-      <div className="header__signature-wrap">
+      <animated.div className="header__signature-wrap" style={{ transform: props.xy.interpolate(signature) }}>
         <Signature />
-      </div>
+      </animated.div>
     </div>
     <div className="header__overlay-wrap">
       <animated.div className="header__overlay header__overlay--2 header__overlay--animated" style={{ transform: props.xy.interpolate(trans3) }}>
