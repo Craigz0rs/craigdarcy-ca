df1b2e3ab66114369f9679afe762a9c7c919e4af
Craigz0rs <cdarcy@live.ca>
2020-04-13 21:33:39 -0600

    wip(aboutme): new svg backgrounds animated

--- portfolio ver3/src/pages/aboutme.js
+++ portfolio ver3/src/pages/aboutme.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
+import Background1 from "../images/background1.svg"
 
 const AboutMe = ({location}) => {
     const fromIndex = () => {
@@ -14,10 +15,12 @@ const AboutMe = ({location}) => {
         <div className="aboutme grid">
             <section className="aboutme__content">
                 <h1 className="aboutme__title content-header">About Me</h1>
-                
+
             </section>
             <div className="aboutme__background-wrap">
-                <div className="aboutme__background"></div>
+                <div className="aboutme__background">
+                    <Background1 />
+                </div>
             </div>
         </div>
       </Layout>
