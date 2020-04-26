2014056034d3c74495fbb100eeccc44f805b0ba0
Craigz0rs <cdarcy@live.ca>
2020-04-25 23:21:26 -0600

    wip(about): animated background

--- portfolio ver3/src/pages/aboutme.js
+++ portfolio ver3/src/pages/aboutme.js
@@ -14,8 +14,18 @@ const AboutMe = ({location}) => {
       <Layout currentPage={location.pathname} index={false} fromIndex={fromIndex()}>
         <div className="aboutme grid">
             <section className="aboutme__content">
-                <h1 className="aboutme__title content-header">About Me</h1>
-
+                <h1 className="aboutme__title content-header text__gradient">About Me</h1>
+                <p>
+                    JAMstack and WordPress developer with a knack for design and an interest in SVG animations.
+                </p>
+                <p>
+                    I've worked with agencies and small businesses to create custom WordPress solutions since 2017. Currently 
+                    working remotely for <a href="http://www.thinkmuse.ca" target="_blank" rel="noopener noreferrer" class="link--inline text__gradient">Think Muse</a> as a front-end developer.
+                </p>
+                <p>
+                    I enjoy building serverless JAMstack sites with React's Gatsby and Vue's Gridsome static site generators, while incorporating WordPress as a familiar
+                    headless CMS solution.
+                </p>
             </section>
             <div className="aboutme__background-wrap">
                 <div className="aboutme__background">
