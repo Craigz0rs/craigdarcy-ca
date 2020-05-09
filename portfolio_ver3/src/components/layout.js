988a23b9b87c6c7fb60d91918dd0fbf19e67cd90
Craigz0rs <cdarcy@live.ca>
2020-05-08 22:12:00 -0700

    test build

--- portfolio_ver3/src/components/layout.js
+++ portfolio_ver3/src/components/layout.js
@@ -1,10 +1,11 @@
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
+import { Location } from '@reach/router';
 import Header from "./header"
 import "./layout.css"
 import "../sass/styles.scss"
+import { globalHistory } from "@reach/router"
 
 const Layout = ({ children, location }) => {
   const data = useStaticQuery(graphql`
@@ -17,24 +18,26 @@ const Layout = ({ children, location }) => {
     }
   `)
 
+  const [thePath, setThePath] = useState("")
+
   // const pageNameCompatibility = () => (currentPage === "/") ? "home" : currentPage.substring(1)
-  const pageNameCompatibility = () => {
-    if (location && location.pathname) {
-      return location.pathname === "/" ? "home" : location.pathname.substring(1)
-    } else {
-      return "404"
-    }
-  }
-  const isIndex = () => (location.pathname === "/") ? true : false
+  // const pageNameCompatibility = () => {
+  //   if (globalHistory && globalHistory.location.pathname) {
+  //     return globalHistory.location.pathname === "/" ? "home" : globalHistory.location.pathname.substring(1)
+  //   } else {
+  //     return "404"
+  //   }
+  // }
+  // const isIndex = () => (globalHistory.location.pathname === "/") ? true : false
 
   return (
     <>
       <Header 
         siteTitle={data.site.siteMetadata.title} 
-        currentPage={pageNameCompatibility()} 
-        index={isIndex()} 
+        // currentPage={pageNameCompatibility()} 
+        // index={isIndex()} 
       />
-      <main className={`main main--${pageNameCompatibility()}`}>
+      <main className={`main main--`}>
         <div className="content-wrap">          
           {children}
         </div>
