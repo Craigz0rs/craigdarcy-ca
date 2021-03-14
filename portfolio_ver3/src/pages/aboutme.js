7f49a0f077351738252461f583fd1b8064a81465
Craigz0rs <cdarcy@live.ca>
2021-03-14 15:17:00 -0700

    update(aboutme): add Studiothink job

--- portfolio_ver3/src/pages/aboutme.js
+++ portfolio_ver3/src/pages/aboutme.js
@@ -14,14 +14,13 @@ const AboutMe = () => {
             <section className="aboutme__content">
                 <h1 className="aboutme__title content-header text__gradient">About Me</h1>
                 <p>
-                    JAMstack and WordPress developer with a knack for design and an interest in SVG animations.
+                    WordPress, Concrete5, and JAMstack developer with a knack for design and an interest in SVG animations.
                 </p>
                 <p>
-                    I've worked with agencies and small businesses to create custom WordPress solutions since 2017. Currently 
-                    working remotely for <a href="http://www.thinkmuse.ca" target="_blank" rel="noopener noreferrer" className="link--inline text__gradient">Think Muse</a> as a front-end developer.
+                    I've worked with agencies and small businesses to create custom WordPress solutions since 2017. Currently working as a developer at <a href="https://studiothink.com" target="_blank" rel="noopener noreferrer" className="link--inline text__gradient">Studiothink</a>.
                 </p>
                 <p>
-                    I enjoy building serverless JAMstack sites with React's Gatsby and Vue's Gridsome static site generators, while incorporating WordPress as a familiar
+                    In my side projects, I enjoy building serverless JAMstack sites with React's Gatsby and Vue's Gridsome static site generators, while incorporating WordPress as a familiar
                     headless CMS solution.
                 </p>
             </section>
